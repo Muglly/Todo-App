648ac596b0718a3e8b2a3f4d9afe588a02d0a4a6
Rafael Pereira de Souza <rpsouza.dev@gmail.com>
2022-10-24 17:49:43 -0300

    ativação de login com firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,19 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyAKjOar1_ny1d-s4MOM_CJYbST5fUlsKjY",
   authDomain: "todolist-833e5.firebaseapp.com",
   projectId: "todolist-833e5",
   storageBucket: "todolist-833e5.appspot.com",
   messagingSenderId: "716250277692",
   appId: "1:716250277692:web:5209e41fd938c8d850e039"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-const auth = getAuth(app);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 
 export { db, auth };
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,63 @@
+import { auth } from "./Firebase";
+import { signInWithEmailAndPassword, signOut } from "firebase/auth"
 
 
 const Header = () => {
 
-    return(
+    let usuario = null;
+
+    const criarConta = (e) => {
+        e.preventDefault();
+
+        let email = document.querySelector("[name=email]").value;
+        let password = document.querySelector("[name=password]").value;
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                document.querySelector(".container-login").style.display = "block";
+                document.querySelector(".login").style.display = "none";
+                alert("logado com sucesso!" + user.email);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage)
+            });
+
+    }
+
+    const sair = (e) => {
+        e.preventDefault()
+
+
+        signOut(auth).then(() => {
+            alert("Deslogado");
+            document.querySelector(".container-login").style.display = "none";
+            document.querySelector(".login").style.display = "block";
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+
+    return (
         <div className="header">
             <div className="login">
                 <h2>Faça o Login no App</h2>
-                <form>
-                    <input type="email" name="email" placeholder="Email"/>
-                    <input type="password" name="password" placeholder="Senha"/>
+                <form onSubmit={(e) => criarConta(e)}>
+                    <input type="email" name="email" placeholder="Email" />
+                    <input type="password" name="password" placeholder="Senha" />
                     <button type="submit" name="entrar">Entrar</button>
                 </form>
             </div>
+
+            <div className="container-login">
+                <h2>Olá, você está logado</h2>
+                <button onClick={(e) => sair(e)}>sair</button>
+
+            </div>
+
         </div>
     )
 
